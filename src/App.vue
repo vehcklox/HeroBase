a0010e183982d39e4ae2956aa053a1d9618d5821
Adrien Maranville <Adrien.Maranville@gmail.com>
2018-06-01 22:44:27 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        title: 'Generate Your Army',
+      };
+    },
+  };
+</script>
